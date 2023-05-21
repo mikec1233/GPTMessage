7bbe6a97aaff2c891b7d29bb579d45527e8bc9c4
Michael Collins <mikec1233@gmail.com>
2023-05-21 00:50:18 -0400

    added function for gpt message

--- openai_api.py
+++ openai_api.py
@@ -7,12 +7,14 @@ load_dotenv()
 openai.organization = "org-6jWqtZzRBRGyorjBskMsFAjj"
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-response = openai.ChatCompletion.create(
-  model="gpt-3.5-turbo",
-  messages=[
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": "Explain the use of an ephemeral port."},
-    ]
-)
 
-print(response['choices'][0]['message']['content'])
+def gpt_response(message_content):
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": message_content},
+        ]
+    )
+
+    return (response['choices'][0]['message']['content'])
